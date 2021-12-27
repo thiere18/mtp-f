dc09f7140142266592949727aa09b0bb6d784bd0
thierno <thiernoibrahimalo@esp.sn>
2021-12-27 17:40:16 +0000

    adding other routes

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Exit in case of error
+set -e
+
+# Build and run containers
+docker-compose up -d
+
+# Hack to wait for postgres container to be up before running alembic migrations
+sleep 5;
+
+# Run migrations
+# docker-compose run --rm api alembic downgrade base
+
+docker-compose run --rm api alembic upgrade head
+
+
+# Create initial data
+# docker-compose run --rm api python3 app/initial_data.py
