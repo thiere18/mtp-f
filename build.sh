21aaf9f5efb556861ac684fd726a818b26453e09
thierno <thiernoibrahimalo@esp.sn>
2021-12-27 17:41:51 +0000

    adding other routes

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Build and run containers
-docker-compose up -d
+docker-compose -f docker-compose-dev.yml up -d
 
 # Hack to wait for postgres container to be up before running alembic migrations
 sleep 5;
@@ -12,7 +12,7 @@ sleep 5;
 # Run migrations
 # docker-compose run --rm api alembic downgrade base
 
-docker-compose run --rm api alembic upgrade head
+docker-compose -f docker-compose-dev.yml run --rm api alembic upgrade head
 
 
 # Create initial data
