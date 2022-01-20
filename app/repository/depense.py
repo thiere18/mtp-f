9d3f96c0591c81205c855b4494502de1a01575db
thierno <thiernoibrahimalo@esp.sn>
2022-01-20 23:01:05 +0000

    depense

---
+++ app/repository/depense.py
@@ -0,0 +1,63 @@
+from fastapi import  Response, status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+from typing import List
+from .. import models, schemas, oauth2
+from ..database import get_db
+
+router = APIRouter(
+    prefix="/api/v1/depenses",
+    tags=['Depenses']
+)
+
+def get_depenses(response: Response,db: Session):
+    depenses =db.query(models.Depense).filter(models.Depense.deleted!=True).all()
+    response.headers["Content-Range"] = f"0-9/{len(depenses)}"
+    response.headers['X-Total-Count'] = '30' 
+    response.headers['Access-Control-Expose-Headers'] = 'Content-Range'
+    return depenses
+
+def get_depenses(db: Session , search: str = ""):
+    return (
+        db.query(models.Depense)
+        .filter(
+            models.Depense.deleted != True,
+            models.Depense.motif.contains(search),
+        )
+        .all()
+    )
+
+def create_depense(post: schemas.Depensecreate, db: Session ):
+    #update magasin montant
+
+    new_depense = models.Depense(**post.dict())
+    db.add(new_depense)
+    db.commit()
+    db.refresh(new_depense)
+
+    return new_depense
+
+def get_depense(id: int, db: Session ):
+    depense = db.query(models.Depense).filter(models.Depense.id == id,models.Depense.deleted!=True).first()
+    if not depense:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"depense with id: {id} was not found")
+    return depense
+def delete_depense(id: int, db: Session = Depends(get_db)):
+    depense_query = db.query(models.Depense).filter(models.Depense.id == id,models.Depense.deleted!=True)
+    depense = depense_query.first()
+    if depense is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"depense with id: {id} does not exist")
+    depense.deleted = True
+    db.commit()
+    return depense #Response(status_code=status.HTTP_204_NO_CONTENT)
+
+def update_depense(id: int, updated_post: schemas.CategoryCreate, db: Session = Depends(get_db)):
+    depense_query = db.query(models.Depense).filter(models.Depense.id == id,models.Depense.deleted!=True)
+    depense = depense_query.first()
+    if depense is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"depense with id: {id} does not exist")
+    depense_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
+    return depense_query.first()
--- app/routers/depense.py
+++ app/routers/depense.py
@@ -1,6 +1,7 @@
 from fastapi import  Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from typing import List
+from app.repository import depense
 from .. import models, schemas, oauth2
 from ..database import get_db
 
@@ -12,83 +13,27 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.DepenseOut],response_model_exclude_none=True)
-def get_depenses(response: Response,db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0,):
-    depenses =db.query(models.Depense).filter(models.Depense.deleted!=True).all()
-    response.headers["Content-Range"] = f"0-9/{len(depenses)}"
-    response.headers['X-Total-Count'] = '30' 
-    response.headers['Access-Control-Expose-Headers'] = 'Content-Range'
-
-    return depenses
+def get_depenses(response: Response,db: Session = Depends(get_db)):
+    return depense.get_depenses(response,db)
 
 @router.get("/search", response_model=List[schemas.DepenseOut])
-def get_depenses(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: str = ""):
-
-    return (
-        db.query(models.Depense)
-        .filter(
-            models.Depense.deleted != True,
-            models.Depense.motif.contains(search),
-        )
-        .all()
-    )
-
+def get_depenses(db: Session = Depends(get_db), search: str = ""):
+    return depense.get_depenses(db, search)
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.DepenseOut)
-def create_depense(post: schemas.Depensecreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    #update magasin montant
-
-    new_depense = models.Depense(**post.dict())
-    db.add(new_depense)
-    db.commit()
-    db.refresh(new_depense)
-
-    return new_depense
+def create_depense(post: schemas.Depensecreate, db: Session = Depends(get_db)):
+    return depense.create_depense(post, db)
 
 
 @router.get("/{id}", response_model=schemas.DepenseOut)
-def get_depense(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-  
-
-    depense = db.query(models.Depense).filter(models.Depense.id == id,models.Depense.deleted!=True).first()
-
-    if not depense:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"depense with id: {id} was not found")
-
-    return depense
-
+def get_depense(id: int, db: Session = Depends(get_db)):
+    return depense.get_depense(id,db)
 
 @router.delete("/{id}", response_model_exclude_none=True)
-def delete_depot(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-
-    depense_query = db.query(models.Depense).filter(models.Depense.id == id,models.Depense.deleted!=True)
-
-    depense = depense_query.first()
-
-    if depense is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"depense with id: {id} does not exist")
-    depense.deleted = True
-    db.commit()
-    return depense #Response(status_code=status.HTTP_204_NO_CONTENT)
-
+def delete_depense(id: int, db: Session = Depends(get_db)):
+    return depense.delete_depense(id, db)
+#Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put("/{id}", response_model=schemas.DepotOut)
-def update_depot(id: int, updated_post: schemas.CategoryCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-
-
-
-    depense_query = db.query(models.Depense).filter(models.Depense.id == id,models.Depense.deleted!=True)
-
-    depense = depense_query.first()
-
-    if depense is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"depense with id: {id} does not exist")
-
-
-    depense_query.update(updated_post.dict(), synchronize_session=False)
-
-    db.commit()
-
-    return depense_query.first()
+def update_depense(id: int, updated_post: schemas.CategoryCreate, db: Session = Depends(get_db)):
+    return depense.update_depense(id,updated_post, db)
