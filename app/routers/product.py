f9eb527d7289d111d7cb7b00fc0485dc573a4a85
thierno <thiernoibrahimalo@esp.sn>
2022-01-24 22:57:10 +0000

    product ended

--- app/routers/product.py
+++ app/routers/product.py
@@ -1,99 +1,35 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from fastapi import Response, status, Depends, APIRouter
 from sqlalchemy.orm import Session
-from typing import List, Optional
-from enum import Enum
-from sqlalchemy import func
-# from sqlalchemy.sql.functions import func
-from .. import models, schemas, oauth2
+from typing import List
+from .. import schemas, oauth2
 from ..database import get_db
-
-from enum import Enum
+from app.repository import product
 router = APIRouter(
     prefix="/api/v1/products",
     tags=['Products']
 )
 
-
-
-
-
 @router.get("/", response_model=List[schemas.ProductOut])
 def get_products(response: Response,db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    products =db.query(models.Product).filter(models.Product.deleted!=True).all()
-
-    response.headers["Content-Range"] = f"0-9/{len(products)}"
-    response.headers['X-Total-Count'] = '30' 
-    response.headers['Access-Control-Expose-Headers'] = 'Content-Range'
-
-    return products
+    return product.get_products(response,db)
 
 @router.get("/", response_model=List[schemas.ProductOut])
 def get_products(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),  search: str= ""):
-
-    return (
-        db.query(models.Product)
-        .filter(
-            models.Product.deleted != True,
-            models.Product.designation.contains(search),
-        )
-        .all()
-    )
-
+    return product.get_products(db, search)
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.ProductOut)
 def create_product(post: schemas.ProductCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
- 
-    new_product = models.Product(quantity_left=post.quantity_init,prix_revient=post.prix_achat+post.frais,**post.dict())
-    db.add(new_product)
-    db.commit()
-    db.refresh(new_product)
-
-    return new_product
+    return product.create_product(post, db)
 
 
 @router.get("/{id}", response_model=schemas.ProductOut)
 def get_product(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-  
-
-    product = db.query(models.Product).filter(models.Product.id == id,models.Product.deleted!=True).first()
-
-    if not product:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"product with id: {id} was not found")
-
-    return product
-
+    return product.get_product(id, db)
 
 @router.delete("/{id}", response_model_exclude_none=True)
 def delete_product(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-
-    product_query = db.query(models.Product).filter(models.Product.id == id,models.Product.deleted!=True)
-
-    product = product_query.first()
-
-    if product is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"product with id: {id} does not exist")
-    product.deleted = True
-    db.commit()
-    return product
+    return product.delete_product(id, db)
 
 @router.put("/{id}", response_model=schemas.ProductOut)
 def update_product(id: int, updated_post: schemas.ProductCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-
-
-
-    product_query = db.query(models.Product).filter(models.Product.id == id,models.Product.deleted!=True)
-
-    product = product_query.first()
-
-    if product is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"product with id: {id} does not exist")
-
-
-    product_query.update(updated_post.dict(), synchronize_session=False)
-
-    db.commit()
-
-    return product_query.first()
+    return product.update_product(id, updated_post, db)
